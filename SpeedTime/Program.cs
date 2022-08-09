d479236849c9cb76c8db9216c041b0743b7307e7
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-09 21:06:40 +0300

    Add SpeedTime

---
+++ SpeedTime/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Длительность видео?");
+double time = Convert.ToDouble(Console.ReadLine());
+double speed = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Скорость просмотра?");
+Console.WriteLine(time / speed);
