5462bddef176dcfb4e5bdfdbc7a9486780f745a1
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-19 21:38:10 +0300

    Add Method sum 1-A

---
+++ Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿// Метод суммы чисел от 1 до А
+
+int Sum(int a)
+{
+    int result = 0;
+    for (int i = 0; i <= a; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+void PrintSum(int a)
+{
+    Console.WriteLine($"Сумма чисел от 1 до {a} равна {Sum(a)}");
+}
+
+PrintSum(7);
+
+
